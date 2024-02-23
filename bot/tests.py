425dd2c9caedf30fe359dc4e05eba7c9333dae3f
Hatchi-Kin <j.needham@hotmail.fr>
2024-02-23 19:27:11 +0100

    remove useless stuff

--- bot/tests.py
+++ bot/tests.py
@@ -2,6 +2,7 @@ import unittest
 import os
 from bot.models import DatabaseManager
 
+
 class TestDatabaseManager(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
@@ -12,25 +13,29 @@ class TestDatabaseManager(unittest.TestCase):
     def tearDownClass(cls):
         os.remove(cls.db_path)
 
-    def test_add_and_remove_participant(self):
+
+    def test_add_participant(self):
         user_id = 'test_user'
         username = 'Test User'
         self.db.add_participant_to_db(user_id, username)
         result = self.db.get_participant_username_from_id(user_id)
         self.assertEqual(result[0], username)
+
+
+    def test_remove_participant(self):
+        user_id = 'test_user'
         self.db.remove_participant_from_db(user_id)
         result = self.db.get_participant_username_from_id(user_id)
         self.assertIsNone(result)
 
-    def test_save_and_get_song(self):
+
+    def test_save_song_and_get_song(self):
         user_id = 'test_user'
         song_link = 'https://www.youtube.com/watch?v=test'
         self.db.save_song(user_id, song_link)
         result = self.db.get_last_song()
-        self.assertEqual(result, (song_link, user_id))
+        self.assertEqual(result[0], song_link)
 
-
-# test if 
 
 
 if __name__ == '__main__':
